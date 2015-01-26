aef90f4540f565aa1459646fc905b5fe46c7f4e0
frankwoo <frankwoo.scc@gmail.com>
2015-01-27 01:00:10 +0800

    (1/26 night) Changes to be committed: 	new file:   dist/css/main.css 	new file:   dist/css/monitor.css 	new file:   dist/js/main.js 	new file:   dist/js/monitor.js 	modified:   route/index.js 	modified:   route/lib.js 	modified:   views/index.html

--- route/index.js
+++ route/index.js
@@ -1,1 +1,16 @@
-module.exports = function(app) {};
+var fs = require('fs');
+var path = require('path');
+var express = require('express');
+var hbs = require('hbs');
+
+var lib = require('./lib');
+
+module.exports = function(app) {
+	app.set('view engine', 'html');
+	app.engine('html', hbs.__express);
+	app.use('/lib', lib);
+	app.use(express.static(path.join(__dirname, '../dist/')));
+	app.get('/', function(req, res) {
+		res.render('index');
+	});
+};
--- route/lib.js
+++ route/lib.js
@@ -3,19 +3,23 @@ var path = require('path');
 var express = require('express');
 
 var root = express.Router();
-var router_videojs = express.Router();
-var router_videojs_contrib_media_sources = express.Router();
-var router_videojs_contrib_hls = express.Router();
-var router_pkcs7 = express.Router();
 
-router_videojs.use(express.static(path.join(__dirname, '../bower_components/video.js/dist/video-js/')));
-router_videojs_contrib_media_sources.use(express.static(path.join(__dirname, '../bower_components/videojs-contrib-hls/node_modules/videojs-contrib-media-sources/src/')));
-router_videojs_contrib_hls.use(express.static(path.join(__dirname, '../bower_components/videojs-contrib-hls/src/')));
-router_pkcs7.use(express.static(path.join(__dirname, '../bower_components/videojs-contrib-hls/node_modules/pkcs7/dist/')));
+//var router_videojs = express.Router();
+//var router_videojs_contrib_media_sources = express.Router();
+//var router_videojs_contrib_hls = express.Router();
+//router_videojs.use(express.static(path.join(__dirname, '../bower_components/video.js/dist/video-js/')));
+//router_videojs_contrib_media_sources.use(express.static(path.join(__dirname, '../bower_components/videojs-contrib-hls/node_modules/videojs-contrib-media-sources/src/')));
+//router_videojs_contrib_hls.use(express.static(path.join(__dirname, '../bower_components/videojs-contrib-hls/src/')));
+//router_pkcs7.use(express.static(path.join(__dirname, '../bower_components/videojs-contrib-hls/node_modules/pkcs7/dist/')));
 
-root.use('/videojs', router_videojs);
-root.use('/videojs-contrib-media-sources', router_videojs_contrib_media_sources);
-root.use('/videojs-contrib-hls', router_videojs_contrib_hls);
-root.use('/pkcs7', router_pkcs7);
+root.use('/videojs', express.Router().use(express.static(path.join(__dirname, '../bower_components/video.js/dist/video-js/'))));
+root.use('/videojs-contrib-media-sources', express.Router().use(express.static(path.join(__dirname, '../bower_components/videojs-contrib-media-sources/src/'))));
+root.use('/videojs-contrib-hls', express.Router().use(express.static(path.join(__dirname, '../bower_components/videojs-contrib-hls/src/'))));
+root.use('/bootstrap', express.Router().use(express.static(path.join(__dirname, '../bower_components/bootstrap/dist/'))));
+root.use('/fullcalendar', express.Router().use(express.static(path.join(__dirname, '../bower_components/fullcalendar/dist/'))));
+root.use('/jquery', express.Router().use(express.static(path.join(__dirname, '../bower_components/jquery/'))));
+root.use('/jquery-ui', express.Router().use(express.static(path.join(__dirname, '../bower_components/jquery-ui/'))));
+root.use('/moment', express.Router().use(express.static(path.join(__dirname, '../bower_components/moment/min/'))));
+root.use('/underscore', express.Router().use(express.static(path.join(__dirname, '../bower_components/underscore/'))));
 	
 module.exports = root;
